fc50e65926fb9c357c86a98a20831208b4866c7b
hadass1234 <66486737+hadass1234@users.noreply.github.com>
2023-01-17 15:08:55 +0100

    a

--- javascript.js
+++ javascript.js
@@ -37,9 +37,9 @@ function resize(elem, percent) {
 	var btnContainer = document.getElementById("myBtnContainer");
 	var btns = btnContainer.getElementsByClassName("btn");
 	for (var i = 0; i < btns.length; i++) {
-  	btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+	  btns[i].addEventListener("click", function(){
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	  });
+	}
