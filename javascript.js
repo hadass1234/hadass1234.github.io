29bf30f0ed56844398f49579da1c602d14aab72e
hadass1234 <66486737+hadass1234@users.noreply.github.com>
2023-01-26 22:12:03 +0100

    me

--- javascript.js
+++ javascript.js
@@ -66,3 +66,19 @@ function resize(elem, percent) {
 
 	// Video
 	document.getElementById('vid').play();
+
+
+
+	// Sticky menu
+	window.onscroll = function() {myFunction()};
+
+	var navbar = document.getElementById("navbarA");
+	var sticky = navbar.offsetTop;
+
+	function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	} else {
+		navbar.classList.remove("sticky");
+	}
+	}
