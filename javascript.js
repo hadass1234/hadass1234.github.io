a94791174887d99c45cb390e1a1ac8069e93cdbb
hadass1234 <66486737+hadass1234@users.noreply.github.com>
2023-01-26 16:40:55 +0100

    remove button

--- javascript.js
+++ javascript.js
@@ -66,21 +66,3 @@ function resize(elem, percent) {
 
 	// Video
 	document.getElementById('vid').play();
-
-
-
-
-	// Button to top
-	// Get the button
-	let mybutton = document.getElementById("myBtnA");
-
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function() {scrollFunction()};
-
-	function scrollFunction() {
-  	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  	} else {
-    mybutton.style.display = "none";
-  	}
-	}
