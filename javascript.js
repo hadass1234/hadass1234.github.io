730ec9b43be476620fdb97981fbc46a07d70a203
hadass1234 <66486737+hadass1234@users.noreply.github.com>
2023-02-11 15:31:35 +0100

    f

--- javascript.js
+++ javascript.js
@@ -94,25 +94,4 @@ function resize(elem, percent) {
   	}
 	
 
-	// Image apper hover	
-		const items = document.querySelectorAll('.item')
-		const image = document.querySelector('imgg')
-
-		items.forEach((el) => {
-		el.addEventListener('mouseover', (e) => {
-			imageData = e.target.getAttribute('data-image')
-			console.log(imageData)
-			e.target.style.zIndex = 99
-			image.setAttribute('src', imageData)
-		})
-		el.addEventListener('mousemove', (e) => {
-			image.style.top = e.clientY + 'px'
-			image.style.left = e.clientX + 'px'
-		})
-		el.addEventListener('mouseleave', (e) => {
-			e.target.style.zIndex = 1
-			image.setAttribute('src', '')
-		})
-		})
-
 
